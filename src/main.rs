34daa8faf5a0ef1d0c520cb95d02db2234d70ef9
stevenewald <stevenewald6@gmail.com>
2023-06-30 00:23:26 -0400

    Added configuration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod lattice_config;
+use lattice_config::CONFIG;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Server Address: {}", CONFIG.server_address);
+    println!("Max Connections: {}", CONFIG.max_connections);
+    println!("Port: {}", CONFIG.port);
 }
