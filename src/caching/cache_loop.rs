ac497dfac7ad98fe6d37f9c7631323cc14f16c31
stevenewald <stevenewald6@gmail.com>
2023-07-20 21:02:18 -0400

    Framework for async caching and sorting

--- src/caching/cache_loop.rs
+++ src/caching/cache_loop.rs
@@ -1,10 +1,31 @@
+use super::caching_data::CachingData;
 use crate::piping::column_update::ColumnUpdate;
 use log::info;
 use tokio::sync::mpsc::UnboundedReceiver as Receiver;
+use tokio::time::{self, Duration, Instant};
+
 pub fn start_cache_rx(mut rx: Receiver<ColumnUpdate>) {
     tokio::spawn(async move {
-        while let Some(data) = rx.recv().await {
-            info!("Cache prox rx {}: {:?}", data.table, data.columns);
+        let mut cache = CachingData::new();
+        let mut interval = time::interval_at(Instant::now(), Duration::from_secs(5));
+        loop {
+            tokio::select! {
+                Some(data) = rx.recv() => {
+                    handle_incoming_data(&mut cache, &data);
+                }
+                _ = interval.tick() => {
+                    sort_and_clean_cache(&mut cache);
+                }
+            }
         }
     });
+}
+
+fn handle_incoming_data(cache: &mut CachingData, data: &ColumnUpdate) {
+    info!("Cache prox rx {}: {:?}", data.table, data.columns);
+    cache.update(&data.table, &data.columns);
+}
+
+fn sort_and_clean_cache(cache: &mut CachingData) {
+    println!("Cleaning cache!");
 }
--- src/caching/caching_data.rs
+++ src/caching/caching_data.rs
@@ -1,6 +1,29 @@
 use std::collections::HashMap;
-struct CachingData {
+pub struct CachingData {
     tables: HashMap<String, TableData>,
+}
+
+impl CachingData {
+    pub fn new() -> Self {
+        CachingData {
+            tables: HashMap::new(),
+        }
+    }
+
+    pub fn update(&mut self, table: &String, columns: &Vec<String>) {
+        let table_data = self.tables.entry(table.to_string()).or_insert(TableData {
+            columns: HashMap::new(),
+        });
+
+        for column in columns {
+            let column_data = table_data
+                .columns
+                .entry(column.to_string())
+                .or_insert(ColumnData { access_freq: 0.0 });
+
+            column_data.access_freq += 1.0;
+        }
+    }
 }
 
 struct TableData {
@@ -9,5 +32,4 @@ struct TableData {
 
 struct ColumnData {
     access_freq: f32,
-    avg_size: u32,
 }
