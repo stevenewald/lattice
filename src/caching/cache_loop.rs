704cb9aca651ee551a37b519776a47d09725f45b
stevenewald <stevenewald6@gmail.com>
2023-07-21 14:49:59 -0400

    Updated caching framework

--- src/caching/cache_loop.rs
+++ src/caching/cache_loop.rs
@@ -9,6 +9,7 @@ pub fn start_cache_rx(mut rx: Receiver<ColumnUpdate>) {
         let mut cache = CachingData::new();
         let mut interval = time::interval_at(Instant::now(), Duration::from_secs(5));
         loop {
+            continue;
             tokio::select! {
                 Some(data) = rx.recv() => {
                     handle_incoming_data(&mut cache, &data);
@@ -27,5 +28,5 @@ fn handle_incoming_data(cache: &mut CachingData, data: &ColumnUpdate) {
 }
 
 fn sort_and_clean_cache(cache: &mut CachingData) {
-    println!("Cleaning cache!");
+    cache.sort_and_clean();
 }
--- src/caching/caching_data.rs
+++ src/caching/caching_data.rs
@@ -1,4 +1,6 @@
+use log::info;
 use std::collections::HashMap;
+
 pub struct CachingData {
     tables: HashMap<String, TableData>,
 }
@@ -13,6 +15,7 @@ impl CachingData {
     pub fn update(&mut self, table: &String, columns: &Vec<String>) {
         let table_data = self.tables.entry(table.to_string()).or_insert(TableData {
             columns: HashMap::new(),
+            ordered_columns: Vec::new(),
         });
 
         for column in columns {
@@ -24,10 +27,51 @@ impl CachingData {
             column_data.access_freq += 1.0;
         }
     }
+
+    pub fn sort_and_clean(&mut self) {
+        for (_, table_data) in self.tables.iter_mut() {
+            let mut columns_to_remove: Vec<String> = Vec::new();
+            for (column, column_data) in table_data.columns.iter_mut() {
+                column_data.access_freq /= 2.0;
+                if column_data.access_freq < 1.0 {
+                    column_data.access_freq = 1.0;
+                }
+                // if column_data.access_freq < 1.0 {
+                // columns_to_remove.push(column.to_string());
+                // }
+            }
+
+            for column in columns_to_remove {
+                table_data.columns.remove(&column);
+            }
+        }
+
+        for (_, table_data) in self.tables.iter_mut() {
+            table_data.ordered_columns = table_data
+                .columns
+                .iter()
+                .map(|(column, _)| column.to_string())
+                .collect();
+            table_data.ordered_columns.sort_by(|a, b| {
+                let a_data = table_data.columns.get(a).unwrap();
+                let b_data = table_data.columns.get(b).unwrap();
+                b_data.access_freq.partial_cmp(&a_data.access_freq).unwrap()
+            });
+        }
+
+        //for each table, print table name and the ordered columns
+        for (table, table_data) in self.tables.iter() {
+            info!(
+                "Table: {} - Columns: {:?}",
+                table, table_data.ordered_columns
+            );
+        }
+    }
 }
 
 struct TableData {
     columns: HashMap<String, ColumnData>,
+    ordered_columns: Vec<String>,
 }
 
 struct ColumnData {
